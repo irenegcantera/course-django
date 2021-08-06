9d3620fd1ac20971ea1e6fa05f68864e7e314c6d
Irene González Cantera <irenegcantera@gmail.com>
2021-08-06 12:25:03 +0200

    Remove my secret key

--- projects-django/project1/project1/settings.py
+++ projects-django/project1/project1/settings.py
@@ -19,14 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&=_)prca3*llv=$5dw9#e86((n8$v+o5e)skvv0t_^dqk&!0n)'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
