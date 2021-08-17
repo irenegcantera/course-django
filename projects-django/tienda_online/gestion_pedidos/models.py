7613b1384c80641812b7753bc8f7da625ed68b17
Irene González Cantera <irenegcantera@gmail.com>
2021-08-17 12:40:51 +0200

    Created the models

--- projects-django/tienda_online/gestion_pedidos/models.py
+++ projects-django/tienda_online/gestion_pedidos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# Clase por cada tabla de la base de datos, no es necesario trabajar con sql
+
+class Clientes(models.Model):
+    # campo y tipo de campo, propiedades
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=7)
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    seccion = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+class Pedidos(models.Model):
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
