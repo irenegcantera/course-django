84f8ade410388cea42248f54ae5fa6596ea09c98
Irene González Cantera <irenegcantera@gmail.com>
2021-08-18 14:05:56 +0200

    Added filters to Pedidos and Articulos tables

--- projects-django/tienda_online/gestion_pedidos/admin.py
+++ projects-django/tienda_online/gestion_pedidos/admin.py
@@ -5,8 +5,16 @@ from gestion_pedidos.models import Clientes, Articulos, Pedidos
 
 class ClientesAdmin(admin.ModelAdmin):
     list_display=('nombre','email','telefono') # información en la tabla
-    search_fields=('nombre','telefono') # buscar registro por un filtro
+    search_fields=('nombre','telefono') # buscar registro por un campo
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter=('seccion',) # filtrar campo
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=('numero','fecha')
+    list_filter=('fecha',) # filtrado por el tiempo que tiene el pedido
+    date_hierarchy='fecha' # filtrado por mes y año
 
 admin.site.register(Clientes, ClientesAdmin)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
