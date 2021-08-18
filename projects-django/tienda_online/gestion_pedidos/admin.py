7344221029566a6762a5b3d9b6be6c3f65bd665e
Irene González Cantera <irenegcantera@gmail.com>
2021-08-18 13:37:29 +0200

    I've used the search_fields method for filtering registers

--- projects-django/tienda_online/gestion_pedidos/admin.py
+++ projects-django/tienda_online/gestion_pedidos/admin.py
@@ -4,7 +4,8 @@ from gestion_pedidos.models import Clientes, Articulos, Pedidos
 # Register your models here.
 
 class ClientesAdmin(admin.ModelAdmin):
-    list_display=('nombre','email','telefono')
+    list_display=('nombre','email','telefono') # información en la tabla
+    search_fields=('nombre','telefono') # buscar registro por un filtro
 
 admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos)
