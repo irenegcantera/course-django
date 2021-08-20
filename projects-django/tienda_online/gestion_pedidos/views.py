1e228dd3c846cbc9ed6b9f2467e226a2458919ef
Irene González Cantera <irenegcantera@gmail.com>
2021-08-20 12:49:25 +0200

    New 'buscar' view shows the result in the new 'resultado_busqueda.html' template

--- projects-django/tienda_online/gestion_pedidos/views.py
+++ projects-django/tienda_online/gestion_pedidos/views.py
@@ -1,5 +1,17 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from gestion_pedidos.models import Articulos
 
 # Create your views here.
 def buscar_producto(request):
     return render(request, "search_product_form.html")
+
+def buscar(request):
+    if request.GET["prd"]:
+        #mensaje="Artículo buscado: %r" % request.GET["prd"]
+        producto=request.GET["prd"]
+        articulos=Articulos.objects.filter(nombre__icontains=producto)
+        return render(request, "resultado_busqueda.html", {"articulos": articulos, "query": producto}) # html que muestra el resultado
+    else:
+        mensaje="No has introducido un artículo"
+    return HttpResponse(mensaje)
