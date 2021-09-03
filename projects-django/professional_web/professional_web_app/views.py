ad5d5e7330f13f4afe24dfeadf8d5bacdd661c8b
Irene González Cantera <irenegcantera@gmail.com>
2021-09-03 20:02:31 +0200

    Initializing my personal project...

---
+++ projects-django/professional_web/professional_web_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+# home: about me and summery about my experience
+# trayectoria profesional: mapa experiencia
+# noticias
+# contacto
+
+def home(request):
+    return HttpResponse
+
+def 
