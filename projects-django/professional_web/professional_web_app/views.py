d5bfd863c075d226e2e67145f0f94da00c257dfd
Irene González Cantera <irenegcantera@gmail.com>
2021-09-07 19:03:51 +0200

    Rendering the html documents

--- projects-django/professional_web/professional_web_app/views.py
+++ projects-django/professional_web/professional_web_app/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home") # lo que muestra
+    return render(request,"professional_web_app/home.html") # lo que muestra
 
 def career(request):
-    return HttpResponse("Carrer")
+    return render(request,"professional_web_app/career.html")
 
 def portfolio(request):
-    return HttpResponse("Portfolio")
+    return render(request,"professional_web_app/portfolio.html")
 
 def news(request):
-    return HttpResponse("News")
+    return render(request,"professional_web_app/news.html")
 
 def contact(request):
-    return HttpResponse("Contact")
+    return render(request,"professional_web_app/contact.html")
