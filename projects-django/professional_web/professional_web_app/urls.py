66a6af045a1a3658ea4eb06b9e70aace0565748b
Irene González Cantera <irenegcantera@gmail.com>
2021-09-07 18:58:32 +0200

    I've include the urls project

---
+++ projects-django/professional_web/professional_web_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from professional_web_app import views
+
+urlpatterns = [
+    path('', views.home, name="Home"),
+    path('career/', views.career, name="Career"),
+    path('portfolio/', views.portfolio, name="Portfolio"),
+    path('news/', views.news, name="News"),
+    path('contact/', views.contact, name="Contact"),
+]
