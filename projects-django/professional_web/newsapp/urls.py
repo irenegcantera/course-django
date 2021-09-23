a5cc16a858b9f3057e7eafd7ed3995061e52269d
Irene González Cantera <irenegcantera@gmail.com>
2021-09-23 12:01:03 +0200

    Filter posts by categories...

--- projects-django/professional_web/newsapp/urls.py
+++ projects-django/professional_web/newsapp/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.news, name="News"),
+    path('category/<int:category_id>/', views.category, name="Category"),
 ]
--- projects-django/professional_web/newsapp/views.py
+++ projects-django/professional_web/newsapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from newsapp.models import Post
+from newsapp.models import Category, Post
 
 # Create your views here.
 def news(request):
     posts=Post.objects.all()
     return render(request,"news/news.html",{"posts" : posts})
+
+def category(request, category_id):
+    category=Category.objects.get(id=category_id)
+    posts=Post.objects.filter(categories=category)
+    return render(request,"news/categories.html",{'category':category,'posts':posts})
