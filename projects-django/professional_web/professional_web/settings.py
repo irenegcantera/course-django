4a9fa1aed30c05ec78fa885e46fbe17d30b0336c
Irene González Cantera <irenegcantera@gmail.com>
2021-09-23 13:01:00 +0200

    update

--- projects-django/professional_web/professional_web/settings.py
+++ projects-django/professional_web/professional_web/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your secret key here'
+SECRET_KEY = 'django-insecure-l_6b!8q0wz7y^v82i+hwm7j18%c0653o^l6f^wx9cfl-vqjsop'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'professional_web_app',
     'servicesapp',
+    'newsapp',
 ]
 
 MIDDLEWARE = [
