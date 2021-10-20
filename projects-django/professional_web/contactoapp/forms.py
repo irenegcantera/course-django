b0a04b0125ceca90ed792345ae9ee7012e4fdc15
Irene González Cantera <irenegcantera@gmail.com>
2021-10-20 23:44:30 +0200

    Givinf format to the form contact

--- projects-django/professional_web/contactoapp/forms.py
+++ projects-django/professional_web/contactoapp/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label="Name", required=True)
     email = forms.EmailField(label="Email", required=True)
-    mensaje = forms.CharField(label="Mensaje")
+    mensaje = forms.CharField(label="Mensaje", widget=forms.Textarea)
