7b1fbef8c0ed4c7840517433a340581047324016
Irene González Cantera <irenegcantera@gmail.com>
2021-10-21 22:46:01 +0200

    Obtening form information and send a feedback to the user

--- projects-django/professional_web/contactoapp/views.py
+++ projects-django/professional_web/contactoapp/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 # Create your views here.
 def contact(request):
     form_contact=ContactForm();
+    # si se envia un contenido lo guardamos
+    if(request.method == 'POST'):
+        form_contact = ContactForm(data=request.POST)
+        # si es valido el contenido
+        if form_contact.is_valid():
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            mensaje = request.POST.get('mensaje')
+            return redirect("/contact/?valid")
+
+
     return render(request,"contacto/contact.html",{'form':form_contact})
